01741881ced03d01ae31e24996ff689992187c0a
Akshata kave <Akshatakave2000>
2025-05-27 19:52:37 +0530

    10th May

---
+++ src/ex_01_Java_Basics/Lab007_Keyword_identifier.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab007_Keyword_identifier {
+    public static void main(String[] args) {
+        // Class, object, datatype, access modifier, pakage, control statemen all these are Keywords.
+        // classes, method, variable are used to identify
+        //Used Alphabet, digit, two special symbol, underscore, first char (Alpha, $, _)    }
+}
