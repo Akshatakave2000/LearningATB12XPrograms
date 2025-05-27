4e00f1c38553002c110ef872c3306b1d93aaec6e
Akshata kave <Akshatakave2000>
2025-05-27 17:45:28 +0530

    Hello world_1

---
+++ src/ex_01_Java_Basics/L0001_HelloWorld.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basics;
+
+public class L0001_HelloWorld {
+
+        public static void main(String[] args) {
+
+
+            System.out.println("Hi, Pramod");
+            // This is Pramod, This code will not executed
+
+            /*
+             * Author :  Pramod Dutta
+             *
+             *
+             * */
+
+
+
+    }
+
+}
