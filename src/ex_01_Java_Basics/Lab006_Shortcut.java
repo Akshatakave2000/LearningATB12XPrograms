c835750f935876cde7807378e2562181a8fcb84f
Akshata kave <Akshatakave2000>
2025-05-27 19:30:57 +0530

    10th May

---
+++ src/ex_01_Java_Basics/Lab006_Shortcut.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab006_Shortcut {
+    public static void main(String[] args) {
+
+      // Shortcut ctrl+D
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
+    }
+}
