8166ed924fc39b9b99f0ca668eb2187e7d6ce125
Akshata kave <Akshatakave2000>
2025-05-27 19:07:25 +0530

    10th May

--- src/ex_01_Java_Basics/Lab005_MultiComment.java
+++ src/ex_01_Java_Basics/Lab005_MultiComment.java
@@ -1,4 +1,18 @@
 package ex_01_Java_Basics;
 
 public class Lab005_MultiComment {
+
+        public static void main(String[] args) {
+            System.out.println("Staring Comment");
+            /**
+             * Student : Akshata kave
+             * Qualification : BE-Computer Engineering
+             * Course :  ATB12x Automation Testing
+             * Learning :  Java
+             *
+             **/
+            System.out.println("Multi Comment");
+            System.out.println("Ending Comment");
+        }
+
 }
