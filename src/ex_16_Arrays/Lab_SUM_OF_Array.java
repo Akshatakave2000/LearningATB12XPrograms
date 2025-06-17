63077900570cbb75adf99c356a19d970daa96689
Akshata kave <Akshatakave2000>
2025-06-17 16:51:32 +0530

    1 June 2025

---
+++ src/ex_16_Arrays/Lab_SUM_OF_Array.java
@@ -0,0 +1,15 @@
+package ex_16_Arrays;
+
+public class Lab_SUM_OF_Array {
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 34, 10};
+        int sum = 0; // ->12 + 34 + 10 ->56
+        for (int i = 0; i < numbers.length ; i++) {
+            sum = sum+numbers[i];
+        }
+
+
+        System.out.println(sum);
+    }
+}
